9a3edbeaee3739db9a3d620f06874acc94359e9f
song1027 <wjsthd20@naver.com>
2022-07-13 22:26:45 +0900

    2022-07-13 커밋

--- app/src/main/java/com/example/schoolfoodlist/MainActivity.java
+++ app/src/main/java/com/example/schoolfoodlist/MainActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     final static String KEY="9ee8be5c3531463f92125c15895e372f";
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     //https://open.neis.go.kr/hub/schoolInfo?KEY=df2ba946955d4d1b8a9b9d767f09e25c&Type=json&pIndex=1&pSize=5&SD_SCHUL_CODE=7091382
     //https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=df2ba946955d4d1b8a9b9d767f09e25c&Type=json&pIndex=1&pSize=5&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7091382&MLSV_FROM_YMD=20220702&MLSV_TO_YMD=20220722
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
         title = findViewById(R.id.title_txt);
         foodListView = findViewById(R.id.food_list_view);
         searchBtn = findViewById(R.id.search_school_btn);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.search_school_btn:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/schoolfoodlist/fragment/SchoolSearchFragment.java
+++ app/src/main/java/com/example/schoolfoodlist/fragment/SchoolSearchFragment.java
@@ -1,7 +1,47 @@
 package com.example.schoolfoodlist.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class SchoolSearchFragment extends Fragment {
+import com.example.schoolfoodlist.R;
 
+public class SchoolSearchFragment extends Fragment implements View.OnClickListener {
+
+    Context mContext;
+    EditText schoolNameEditor;
+    ImageView searchBtn;
+    RecyclerView schoolListView;
+
+    public SchoolSearchFragment(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.search_fragment, container, false);
+        schoolListView = view.findViewById(R.id.school_list_view);
+        searchBtn = view.findViewById(R.id.school_search_btn);
+        schoolNameEditor = view.findViewById(R.id.school_name_edit_view);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.school_search_btn:
+                break;
+        }
+    }
 }
