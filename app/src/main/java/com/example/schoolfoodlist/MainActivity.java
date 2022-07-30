a8ab292ea6f9cf55e334cffec3ab7cff3bc58bb7
송윤선 <wjsthd10@gmail.com>
2022-07-30 18:33:43 +0900

    2022-07-30 알러지목록 변환 기능 추가

--- app/src/main/java/com/example/schoolfoodlist/MainActivity.java
+++ app/src/main/java/com/example/schoolfoodlist/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.schoolfoodlist;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.SurfaceControl;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.schoolfoodlist.adpater.FoodListAdapter;
-import com.example.schoolfoodlist.fragment.SchoolSearchFragment;
+import com.example.schoolfoodlist.adpater.SchoolListAdapter;
 import com.example.schoolfoodlist.item.FoodListItem;
 import com.example.schoolfoodlist.item.SchoolDataItem;
 
@@ -40,16 +41,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     final static int CALL_FOOD_LIST = 20001;
     final static int FOOD_LIST_SET = 20002;
 
+    final static int SEARCH_FINISH = 10001;
+    final static int SEARCH_FAILED = 10002;
+    final static int SELECT_SCHOOL = 10003;
+
     TextView title;
     RecyclerView foodListView;
     ImageView searchBtn;
     LinearLayout mainProgress;
+    ConstraintLayout rootView;
     ArrayList<FoodListItem> foodListItems = new ArrayList<>();
+    AlertDialog schoolListDialog;
 
     String MLSV_FROM_YMD="20220701";// 날짜 정보 한달치 표시하기
     String MLSV_TO_YMD="20220729";
 
+    final static String schoolAddress = "https://open.neis.go.kr/hub/schoolInfo";
     //https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=df2ba946955d4d1b8a9b9d767f09e25c&Type=json&pIndex=1&pSize=5&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7091382&MLSV_FROM_YMD=20220702&MLSV_TO_YMD=20220722
+
+    ArrayList<SchoolDataItem> schoolDataItems = new ArrayList<>();
+
+
+    RecyclerView schoolListView;
+    SchoolListAdapter schoolAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setLayout(){
+        rootView = findViewById(R.id.root_view);
         title = findViewById(R.id.title_txt);
         foodListView = findViewById(R.id.food_list_view);
         searchBtn = findViewById(R.id.search_school_btn);
@@ -73,13 +88,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.search_school_btn:
-                SchoolSearchFragment schoolSearchFragment = new SchoolSearchFragment(handlerMain);
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.root_view, schoolSearchFragment).addToBackStack(null).commitAllowingStateLoss();
+            case R.id.search_school_btn:                ConstraintLayout dialogView = (ConstraintLayout) LayoutInflater.from(this).inflate(R.layout.search_fragment, rootView, false);
+
+                schoolListView = dialogView.findViewById(R.id.school_list_view);
+                ImageView schoolSearchBtn = dialogView.findViewById(R.id.school_search_btn);
+                EditText schoolNameEditor = dialogView.findViewById(R.id.school_name_edit_view);
+
+                schoolSearchBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mainProgress.setVisibility(View.VISIBLE);
+                        String schoolName = schoolNameEditor.getText().toString();
+                        if (schoolName.equals("") || schoolName.contains("-") || schoolName.contains(".") || schoolName.contains(",") || schoolName.equals(" ")){
+                            mainProgress.setVisibility(View.GONE);
+                            return;
+                        }
+                        String address = schoolAddress
+                                +"?KEY="+KEY
+                                +"&Type=json&pIndex=1&pSize=5"
+                                +"&SCHUL_NM="+schoolName;
+                        getSchoolName(address);
+                    }
+                });
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setView(dialogView);
+                schoolListDialog = builder.create();
+                schoolListDialog.show();
+
                 Log.e("yun_log", "is start fragment");
                 break;
-
         }
     }
 
@@ -112,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                     buffer.append(line+"\n");
                                     line = reader.readLine();
                                 }
-
+//1.난류, 2.우유, 3.메밀, 4.땅콩, 5.대두, 6.밀, 7.고등어, 8.게, 9.새우, 10.돼지고기, 11.복숭아, 12.토마토, 13.아황산염, 14.호두, 15.닭고기, 16.쇠고기, 17.오징어, 18.조개류(굴,전복,홍합 등)
                                 String jsonData = buffer.toString();
                                 JSONObject obj = new JSONObject(jsonData);
                                 JSONArray obj2 = obj.getJSONArray("mealServiceDietInfo");
@@ -124,8 +162,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                         for (int j = 0; j < rowArr.length(); j++) {
                                             JSONObject foodData = rowArr.getJSONObject(j);
                                             String day= foodData.getString("MLSV_FROM_YMD");
-                                            String list = foodData.getString("DDISH_NM").replace("<br/>", "\n");
-                                            foodListItems.add(new FoodListItem(day,list));
+                                            String list = foodData.getString("DDISH_NM");
+
+                                            foodListItems.add(new FoodListItem(day,replaceStr(list)));
                                         }
                                     }
                                 }
@@ -157,12 +196,113 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     FoodListAdapter adapter = new FoodListAdapter(MainActivity.this, foodListItems);
                     foodListView.setAdapter(adapter);
                     break;
+
+                case SEARCH_FINISH:
+                    mainProgress.setVisibility(View.GONE);
+                    schoolAdapter = new SchoolListAdapter(MainActivity.this, schoolDataItems, handlerMain);
+                    schoolListView.setAdapter(schoolAdapter);
+                    schoolAdapter.notifyDataSetChanged();
+                    break;
+                case SEARCH_FAILED:
+                    break;
+                case SELECT_SCHOOL:
+                    mainProgress.setVisibility(View.GONE);
+                    SchoolDataItem items = (SchoolDataItem) msg.obj;
+                    Message message = new Message();
+                    message.what = CALL_FOOD_LIST;
+                    message.obj = items;
+                    handlerMain.sendMessage(message);
+                    schoolListDialog.dismiss();
+                    break;
             }
             return false;
         }
     });
 
+    private String replaceStr(String list){
+        String retVal=list;
+        retVal = retVal.replace("<br/>", "\n");
+        retVal = retVal.replace("(", "\n(");
+        if (retVal.contains("18.")) { retVal = retVal.replace("18.","조개류[굴,전복,홍합 등]"); }
+        if (retVal.contains("17.")) { retVal = retVal.replace("17.","오징어."); }
+        if (retVal.contains("16.")) { retVal = retVal.replace("16.","쇠고기."); }
+        if (retVal.contains("15.")) { retVal = retVal.replace("15.","닭고기."); }
+        if (retVal.contains("14.")) { retVal = retVal.replace("14.","호두."); }
+        if (retVal.contains("13.")) { retVal = retVal.replace("13.","아황산염."); }
+        if (retVal.contains("12.")) { retVal = retVal.replace("12.","토마토."); }
+        if (retVal.contains("11.")) { retVal = retVal.replace("11.","복숭아."); }
+        if (retVal.contains("10.")) { retVal = retVal.replace("10.","돼지고기."); }
+        if (retVal.contains("9.")) { retVal = retVal.replace("9.","새우."); }
+        if (retVal.contains("8.")) { retVal = retVal.replace("8.","게."); }
+        if (retVal.contains("7.")) { retVal = retVal.replace("7.","고등어."); }
+        if (retVal.contains("6.")) { retVal = retVal.replace("6.","밀."); }
+        if (retVal.contains("5.")) { retVal = retVal.replace("5.","대두."); }
+        if (retVal.contains("4.")) { retVal = retVal.replace("4.","땅콩."); }
+        if (retVal.contains("3.")) { retVal = retVal.replace("3.","메밀."); }
+        if (retVal.contains("2.")) { retVal = retVal.replace("2.","우유."); }
+        if (retVal.contains("1.")) { retVal = retVal.replace("1.","난류."); }
 
+        return retVal;
+    }
 
+    private void getSchoolName(String address){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(address);
+                    InputStream is = url.openStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader reader = new BufferedReader(isr);
 
+                    StringBuffer buffer = new StringBuffer();
+                    String line = reader.readLine();
+                    while (line != null) {
+                        buffer.append(line+"\n");
+                        line = reader.readLine();
+                    }
+
+                    String jsonData = buffer.toString();
+                    JSONObject obj = new JSONObject(jsonData);
+                    JSONArray obj2 = obj.getJSONArray("schoolInfo");
+                    schoolDataItems.clear();
+                    for (int i = 0; i < obj2.length(); i++) {
+                        if (obj2.get(i).toString().contains("row")){
+                            JSONObject row = obj2.getJSONObject(i);
+                            JSONArray rowArr = row.getJSONArray("row");
+                            for (int j = 0; j < rowArr.length(); j++) {
+                                JSONObject schoolData = rowArr.getJSONObject(j);
+                                SchoolDataItem item = new SchoolDataItem();
+                                item.setsName(schoolData.getString("SCHUL_NM"));
+                                item.setsCode(schoolData.getString("SD_SCHUL_CODE"));
+                                item.setAddress(schoolData.getString("ORG_RDNMA"));
+                                item.setAtptCode(schoolData.getString("ATPT_OFCDC_SC_CODE"));
+                                schoolDataItems.add(item);
+                            }
+                        }
+                    }
+
+                    Message msg = new Message();
+                    msg.what=SEARCH_FINISH;
+                    handlerMain.sendMessage(msg);
+
+                } catch (MalformedURLException e) {
+                    searchFailed();
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    searchFailed();
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    searchFailed();
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void searchFailed(){
+        Message msg = new Message();
+        msg.what=SEARCH_FAILED;
+        handlerMain.sendMessage(msg);
+    }
 }
